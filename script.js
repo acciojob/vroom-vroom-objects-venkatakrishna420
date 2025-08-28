2a8ad3fa9fb3eea0955b03acc1a7376f3d749dc5
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-08-28 13:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	if(typeof make !== "string" || typeof model !=="string"){
+		throw new error("make and model should be string")
+	}
+	this.make=make;
+	this.model=model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model}`
+}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+function SportsCar(make, model, topSpeed) {
+	this.make=make;
+	this.model=model;
+	this.topSpeed=topSpeed;
+}
+
+SportsCar.prototype=Object.create(Car.prototype)
+SportsCar.prototype.constructor=SportsCar
+SportsCar.prototype.getTopSpeed=function(){
+    return this.topSpeed;
+}
+
+const car=new SportsCar("ferrai","0239",200)
+console.log(car.getMakeModel())
+console.log(car.getTopSpeed())
